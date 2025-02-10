cba8a67f144f45a3c7e05086109d82a42a58f76d
Sai Yan Naing <pesilay2372002@gmail.com>
2025-02-10 15:23:12 +0630

    shadcn init and dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { ModeToggle } from '@/components/ModeToggle';
+import { Button } from '@/components/ui/button';
 import {
   SignInButton,
   SignedIn,
@@ -8,11 +10,16 @@ export default function Home() {
   return (
     <>
     <SignedOut>
-      <SignInButton mode='modal' />
+      <SignInButton mode='modal'>
+        <Button className='bg-emerald-700 hover:bg-emerald-400 ' >
+          Sign In
+        </Button>
+      </SignInButton>
     </SignedOut>
     <SignedIn>
       <UserButton />
     </SignedIn>
+    <ModeToggle/>
     </>
   );
 }
